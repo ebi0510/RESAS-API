2c0ae4dea6a5325ff79cfe44df03852ce2b1dab4
haruna <haruna@chatwork.com>
2023-05-11 18:17:09 +0900

    コメント記載

--- checkbox.js
+++ checkbox.js
@@ -5,7 +5,11 @@ function clickBtn1() {
     for (let i = 0; i < chk1.length; i++) {
       if (chk1[i].checked) {//(chk1[i].checked === true)と同じ
         arr.push(chk1[i].id);
+        // arrにチェックが入った都道府県のidを代入
       }
     }
     document.getElementById("span1").textContent = arr;
+    // span1にテキストデータ（arr）を表示
+    p_id = arr.toString();
+    // 配列だと文字列と連拳できないので変換
   }

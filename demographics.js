5411f67380a3ced266f587a95152bf6e032ca659
haruna <haruna@chatwork.com>
2023-05-11 18:18:27 +0900

    取得したidによってデータ取得先のURLを変更する（よくわかんないけど動いてる）

--- demographics.js
+++ demographics.js
@@ -1,12 +1,16 @@
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      'X-API-KEY': 'G5o0d05drMR4MXloFtNDv2u2eqE8oK2SX5SiNzXL'
-    }
-  };
-  
-  fetch('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=11', options)
-    .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
-    .catch(err => console.error(err));
+function arr(np_id){
+    // np_idなのになんでp_idのデータ使えてるの？
+        const options = {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        'X-API-KEY': 'G5o0d05drMR4MXloFtNDv2u2eqE8oK2SX5SiNzXL'
+        }
+    };
+    
+    fetch('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode='+p_id, options)
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
+        .catch(err => console.error(err));
+        console.log('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode='+p_id)
+}

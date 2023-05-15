83fefa137a58f6759e3a28ab559db41d7ebb9ba3
haruna <haruna@chatwork.com>
2023-05-15 17:45:53 +0900

    checkbox.jsの結果を引用できるように変更

--- demographics.js
+++ demographics.js
@@ -1,16 +1,16 @@
-function arr(np_id){
-    // np_idなのになんでp_idのデータ使えてるの？
-        const options = {
+function population_stat(){
+    // index.htmlでonclick="dgp(check_radiobtn());"と書いた場合、function population_stat(data){}みたいに引数が使える
+    var data = check_radiobtn();
+    const options = {
         method: 'GET',
         headers: {
-        accept: 'application/json',
-        'X-API-KEY': 'G5o0d05drMR4MXloFtNDv2u2eqE8oK2SX5SiNzXL'
+            accept: 'application/json',
+            'X-API-KEY': 'G5o0d05drMR4MXloFtNDv2u2eqE8oK2SX5SiNzXL'
         }
     };
     
-    fetch('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode='+p_id, options)
+    fetch('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode='+data, options)
         .then(response => response.json())
         .then(response => console.log(JSON.stringify(response)))
         .catch(err => console.error(err));
-        console.log('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode='+p_id)
 }

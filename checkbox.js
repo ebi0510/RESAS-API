3e49c6cdb33583a37b8d76b51323ad1cd017a7c8
haruna <haruna@chatwork.com>
2023-05-15 17:47:04 +0900

    配列から文字列へ変更。それに伴い微修正。

--- checkbox.js
+++ checkbox.js
@@ -1,15 +1,15 @@
-function clickBtn1() {
-    const arr = [];
-    const chk1 = document.form.check;
+function check_radiobtn() {
+    let chk;
+    const radio = document.form.check;
 
-    for (let i = 0; i < chk1.length; i++) {
-      if (chk1[i].checked) {//(chk1[i].checked === true)と同じ
-        arr.push(chk1[i].id);
+    for (let i = 0; i < radio.length; i++) {
+      if (radio[i].checked) {//(chk1[i].checked === true)と同じ
+        chk = radio[i].id;
         // arrにチェックが入った都道府県のidを代入
       }
     }
-    document.getElementById("span1").textContent = arr;
-    // span1にテキストデータ（arr）を表示
-    p_id = arr.toString();
-    // 配列だと文字列と連拳できないので変換
+    document.getElementById("span1").textContent = chk;
+    // span1にテキストデータ（chk）を表示
+    p_id = chk;
+    return p_id;
   }

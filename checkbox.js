5df7fb294f20ec40d2e09bc9ec52e7df5bea1009
haruna <haruna@chatwork.com>
2023-06-12 16:01:31 +0900

    関数名変更

--- checkbox.js
+++ checkbox.js
@@ -1,4 +1,4 @@
-function check_radiobtn() {
+function get_prfecture_id() {
     let chk;
     const radio = document.form.check;
     // これってなんのためにあるの？
@@ -8,16 +8,16 @@ function check_radiobtn() {
       if (radio[i].checked) {//(chk1[i].checked === true)と同じ
         chk = radio[i].id;
         // chkにチェックが入った都道府県のidを代入
+        document.getElementById("prefecture").textContent = radio[i].value;
       }
     }
-    document.getElementById("span1").textContent = chk;
-    // span1にテキストデータ（chk）を表示
+
     p_id = chk;
     console.log(p_id);
     return p_id;
   }
 
-  function check_populationbtn() {
+  function get_population_id() {
     let chp;
     const btn = document.btn1.check;
     // これってなんのためにあるの？
@@ -26,10 +26,10 @@ function check_radiobtn() {
     for (let i = 0; i < btn.length; i++) {
       if (btn[i].checked) {//(chk1[i].checked === true)と同じ
         chp = btn[i].id;
+        document.getElementById("data_type").textContent = btn[i].value;
         // chkにチェックが入った都道府県のidを代入
       }
     }
-    document.getElementById("span2").textContent = chp;
 
     chp_id = chp;
     console.log(chp_id);
